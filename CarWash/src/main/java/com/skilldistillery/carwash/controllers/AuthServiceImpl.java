7f40c563f8effa7f6bf2ea2790b16f69b55b98da
diego.delapuente35 <diego.a.delapuente@gmail.com>
2024-01-24 10:49:54 -0800

    stub authentication

---
+++ CarWash/src/main/java/com/skilldistillery/carwash/controllers/AuthServiceImpl.java
@@ -0,0 +1,33 @@
+package com.skilldistillery.carwash.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.skilldistillery.loghome.entities.User;
+import com.skilldistillery.loghome.repositories.UserRepository;
+
+@Service
+public class AuthServiceImpl implements AuthService {
+
+	@Autowired
+	private UserRepository userRepo;
+	
+	@Autowired
+	private PasswordEncoder encoder;
+	
+	@Override
+	public User register(User user) {
+//		user.setEnabled(true);
+//		user.setRole("standard");
+//		String encryptedPassword = encoder.encode(user.getPassword());
+//		user.setPassword(encryptedPassword);
+		return userRepo.saveAndFlush(user);
+	}
+
+	@Override
+	public User getUserByUsername(String username) {
+		return userRepo.findByUsername(username);
+	}
+
+}
