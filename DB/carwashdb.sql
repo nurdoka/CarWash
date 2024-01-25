69fc99945c9234ab5d0fab105614b3f7581648da
Nurdoka <nurdoolot93@gmail.com>
2024-01-26 02:34:30 +0600

    create data for DB

--- DB/carwashdb.sql
+++ DB/carwashdb.sql
@@ -16,6 +16,21 @@ CREATE SCHEMA IF NOT EXISTS `carwashdb` DEFAULT CHARACTER SET utf8 ;
 USE `carwashdb` ;
 
 -- -----------------------------------------------------
+-- Table `address`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `address` ;
+
+CREATE TABLE IF NOT EXISTS `address` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `street` VARCHAR(45) NULL,
+  `city` VARCHAR(45) NULL,
+  `state` VARCHAR(45) NULL,
+  `zip` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
 -- Table `user`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `user` ;
@@ -29,10 +44,19 @@ CREATE TABLE IF NOT EXISTS `user` (
   `password` VARCHAR(100) NOT NULL,
   `enabled` TINYINT NOT NULL,
   `role` VARCHAR(45) NOT NULL,
+  `address_id` INT NOT NULL,
+  `create_date` DATETIME NULL,
+  `last_update` DATETIME NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC),
   UNIQUE INDEX `user_name_UNIQUE` (`username` ASC),
-  UNIQUE INDEX `email_UNIQUE` (`email` ASC))
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC),
+  INDEX `fk_user_address1_idx` (`address_id` ASC),
+  CONSTRAINT `fk_user_address1`
+    FOREIGN KEY (`address_id`)
+    REFERENCES `address` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
@@ -45,9 +69,13 @@ CREATE TABLE IF NOT EXISTS `vehicle` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `make` VARCHAR(45) NULL,
   `model` VARCHAR(45) NULL,
-  `year` VARCHAR(45) NULL,
+  `year` INT NULL,
   `user_id` INT NOT NULL,
   `license_plate` VARCHAR(45) NOT NULL,
+  `create_date` DATETIME NULL,
+  `last_update` DATETIME NULL,
+  `enabled` TINYINT NULL,
+  `color` VARCHAR(45) NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_VEHICLE_USER_idx` (`user_id` ASC),
   CONSTRAINT `fk_VEHICLE_USER`
@@ -65,31 +93,47 @@ DROP TABLE IF EXISTS `store` ;
 
 CREATE TABLE IF NOT EXISTS `store` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `address` VARCHAR(45) NULL,
-  `user_id` INT NOT NULL,
+  `manager_id` INT NOT NULL,
+  `phone` VARCHAR(45) NULL,
+  `email` VARCHAR(45) NULL,
+  `name` VARCHAR(45) NOT NULL,
+  `address_id` INT NOT NULL,
+  `create_date` DATETIME NULL,
+  `last_update` DATETIME NULL,
+  `enabled` TINYINT NULL,
+  `image_url` VARCHAR(2000) NULL,
+  `description` TEXT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_store_user1_idx` (`user_id` ASC),
+  INDEX `fk_store_user1_idx` (`manager_id` ASC),
+  INDEX `fk_store_address1_idx` (`address_id` ASC),
   CONSTRAINT `fk_store_user1`
-    FOREIGN KEY (`user_id`)
+    FOREIGN KEY (`manager_id`)
     REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_store_address1`
+    FOREIGN KEY (`address_id`)
+    REFERENCES `address` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `pricing`
+-- Table `service_level`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `pricing` ;
+DROP TABLE IF EXISTS `service_level` ;
 
-CREATE TABLE IF NOT EXISTS `pricing` (
+CREATE TABLE IF NOT EXISTS `service_level` (
   `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL,
+  `description` TEXT NULL,
+  `image_url` VARCHAR(2000) NULL,
+  `cost_per_wash` DECIMAL(5,2) NULL,
   `store_id` INT NOT NULL,
-  `pricing_type` VARCHAR(45) NULL,
-  `cost` DECIMAL NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_pricing_store1_idx` (`store_id` ASC),
-  CONSTRAINT `fk_pricing_store1`
+  INDEX `fk_service_level_store1_idx` (`store_id` ASC),
+  CONSTRAINT `fk_service_level_store1`
     FOREIGN KEY (`store_id`)
     REFERENCES `store` (`id`)
     ON DELETE NO ACTION
@@ -98,20 +142,51 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `car_wash_log`
+-- Table `membership`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `car_wash_log` ;
+DROP TABLE IF EXISTS `membership` ;
 
-CREATE TABLE IF NOT EXISTS `car_wash_log` (
+CREATE TABLE IF NOT EXISTS `membership` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `vehicle_id` INT NOT NULL,
+  `service_level_id` INT NOT NULL,
+  `create_date` DATETIME NULL,
+  `expiration_date` DATETIME NULL,
+  `cost_per_wash` DECIMAL(5,2) NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_membership_vehicle1_idx` (`vehicle_id` ASC),
+  INDEX `fk_membership_service_level1_idx` (`service_level_id` ASC),
+  CONSTRAINT `fk_membership_vehicle1`
+    FOREIGN KEY (`vehicle_id`)
+    REFERENCES `vehicle` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_membership_service_level1`
+    FOREIGN KEY (`service_level_id`)
+    REFERENCES `service_level` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `wash`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `wash` ;
+
+CREATE TABLE IF NOT EXISTS `wash` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `vehicle_id` INT NOT NULL,
   `store_id` INT NOT NULL,
   `create_date` DATETIME NULL,
-  `pricing_id` INT NOT NULL,
+  `price_paid` DECIMAL(5,2) NULL,
+  `membership_id` INT NULL,
+  `service_level_id` INT NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_car_wash_log_vehicle1_idx` (`vehicle_id` ASC),
   INDEX `fk_car_wash_log_store1_idx` (`store_id` ASC),
-  INDEX `fk_car_wash_log_pricing1_idx` (`pricing_id` ASC),
+  INDEX `fk_wash_membership1_idx` (`membership_id` ASC),
+  INDEX `fk_wash_service_level1_idx` (`service_level_id` ASC),
   CONSTRAINT `fk_car_wash_log_vehicle1`
     FOREIGN KEY (`vehicle_id`)
     REFERENCES `vehicle` (`id`)
@@ -122,9 +197,68 @@ CREATE TABLE IF NOT EXISTS `car_wash_log` (
     REFERENCES `store` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_car_wash_log_pricing1`
-    FOREIGN KEY (`pricing_id`)
-    REFERENCES `pricing` (`id`)
+  CONSTRAINT `fk_wash_membership1`
+    FOREIGN KEY (`membership_id`)
+    REFERENCES `membership` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_wash_service_level1`
+    FOREIGN KEY (`service_level_id`)
+    REFERENCES `service_level` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `comment`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `comment` ;
+
+CREATE TABLE IF NOT EXISTS `comment` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `content` TEXT NULL,
+  `store_id` INT NOT NULL,
+  `user_id` INT NOT NULL,
+  `comment_date` DATETIME NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_comment_store1_idx` (`store_id` ASC),
+  INDEX `fk_comment_user1_idx` (`user_id` ASC),
+  CONSTRAINT `fk_comment_store1`
+    FOREIGN KEY (`store_id`)
+    REFERENCES `store` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_comment_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `store_rating`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `store_rating` ;
+
+CREATE TABLE IF NOT EXISTS `store_rating` (
+  `user_id` INT NOT NULL,
+  `store_id` INT NOT NULL,
+  `rating` INT NULL,
+  `create_date` DATETIME NULL,
+  `rating_comment` TEXT NULL,
+  PRIMARY KEY (`user_id`, `store_id`),
+  INDEX `fk_user_has_store_store1_idx` (`store_id` ASC),
+  INDEX `fk_user_has_store_user1_idx` (`user_id` ASC),
+  CONSTRAINT `fk_user_has_store_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_has_store_store1`
+    FOREIGN KEY (`store_id`)
+    REFERENCES `store` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -141,11 +275,91 @@ SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
 -- -----------------------------------------------------
+-- Data for table `address`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `carwashdb`;
+INSERT INTO `address` (`id`, `street`, `city`, `state`, `zip`) VALUES (1, '123 Fake st', 'Denver', 'CO', '80913');
+
+COMMIT;
+
+
+-- -----------------------------------------------------
 -- Data for table `user`
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `carwashdb`;
-INSERT INTO `user` (`id`, `first_name`, `last_name`, `email`, `username`, `password`, `enabled`, `role`) VALUES (1, 'John', 'Doe', 'some@gmail.com', 'admin', '$2a$10$nShOi5/f0bKNvHB8x0u3qOpeivazbuN0NE4TO0LGvQiTMafaBxLJS', 1, 'CUSTOMER');
+INSERT INTO `user` (`id`, `first_name`, `last_name`, `email`, `username`, `password`, `enabled`, `role`, `address_id`, `create_date`, `last_update`) VALUES (1, 'John', 'Doe', 'some@gmail.com', 'admin', '$2a$10$nShOi5/f0bKNvHB8x0u3qOpeivazbuN0NE4TO0LGvQiTMafaBxLJS', 1, 'CUSTOMER', 1, NULL, NULL);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `vehicle`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `carwashdb`;
+INSERT INTO `vehicle` (`id`, `make`, `model`, `year`, `user_id`, `license_plate`, `create_date`, `last_update`, `enabled`, `color`) VALUES (1, 'Ford', 'F150', 2023, 1, '1234', NULL, NULL, 1, 'green');
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `store`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `carwashdb`;
+INSERT INTO `store` (`id`, `manager_id`, `phone`, `email`, `name`, `address_id`, `create_date`, `last_update`, `enabled`, `image_url`, `description`) VALUES (1, 1, '720-100-2000', 'some@gmail.com', 'CarWash', 1, NULL, NULL, 1, 'somepic.com', 'best store');
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `service_level`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `carwashdb`;
+INSERT INTO `service_level` (`id`, `name`, `description`, `image_url`, `cost_per_wash`, `store_id`) VALUES (1, 'basic', 'basic wash', NULL, 8.99, 1);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `membership`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `carwashdb`;
+INSERT INTO `membership` (`id`, `vehicle_id`, `service_level_id`, `create_date`, `expiration_date`, `cost_per_wash`) VALUES (1, 1, 1, NULL, NULL, 8.99);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `wash`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `carwashdb`;
+INSERT INTO `wash` (`id`, `vehicle_id`, `store_id`, `create_date`, `price_paid`, `membership_id`, `service_level_id`) VALUES (1, 1, 1, NULL, 8.99, 1, 1);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `comment`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `carwashdb`;
+INSERT INTO `comment` (`id`, `content`, `store_id`, `user_id`, `comment_date`) VALUES (1, 'cool place', 1, 1, NULL);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `store_rating`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `carwashdb`;
+INSERT INTO `store_rating` (`user_id`, `store_id`, `rating`, `create_date`, `rating_comment`) VALUES (1, 1, 5, NULL, 'great');
 
 COMMIT;
 
