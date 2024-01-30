1b43ed08e31a11fa58d92ccd0d7c36730626c392
diego.delapuente35 <diego.a.delapuente@gmail.com>
2024-01-30 12:23:13 -0800

    correcting merge issues of service registerNewVehicle

--- CarWash/src/main/java/com/skilldistillery/carwash/repositories/VehicleRepository.java
+++ CarWash/src/main/java/com/skilldistillery/carwash/repositories/VehicleRepository.java
@@ -3,6 +3,8 @@ package com.skilldistillery.carwash.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.skilldistillery.carwash.entities.Vehicle;
 
--- CarWash/src/main/java/com/skilldistillery/carwash/services/VehicleService.java
+++ CarWash/src/main/java/com/skilldistillery/carwash/services/VehicleService.java
@@ -7,12 +7,9 @@ import com.skilldistillery.carwash.entities.Vehicle;
 public interface VehicleService {
 	public List<Vehicle> returnAllVehicles();
 	public List<Vehicle> returnAllVehiclesByUser(String userName);
-<<<<<<< HEAD
-	public Vehicle registerNewVehicle(Vehicle vehicle);
+
+	public Vehicle registerNewVehicle(Vehicle vehicle,String username);
 	public Vehicle findById(int id);
 	public Vehicle updateVehicle(Vehicle vehicle);
-=======
-	public Vehicle registerNewVehicle(Vehicle vehicle,String username);
 
->>>>>>> 61f1877ac5e58ef37d78cc0a60bdf9e6674b0d5f
 }
