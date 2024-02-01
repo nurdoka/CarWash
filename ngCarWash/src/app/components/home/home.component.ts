e026133fd65504f7b33adc4e72f8bb28d439ac3d
Nurdoka <nurdoolot93@gmail.com>
2024-02-01 18:17:45 +0600

    homepage backgroud set

--- ngCarWash/src/app/components/home/home.component.ts
+++ ngCarWash/src/app/components/home/home.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { LoginComponent } from '../login/login.component';
+import { RegisterComponent } from '../register/register.component';
+import { AuthService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    LoginComponent,
+    RegisterComponent,
+    CommonModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  constructor(private auth: AuthService){}
+
+  checkLogin():boolean{
+    return this.auth.checkLogin();
+  }
 
 }
